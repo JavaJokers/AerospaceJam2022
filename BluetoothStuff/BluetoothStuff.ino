8b404aabdd5e84519dc9878a81434ce3ce0f0214
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2022-02-22 11:26:25 -0600

    Update BluetoothStuff.ino

--- BluetoothStuff/BluetoothStuff.ino
+++ BluetoothStuff/BluetoothStuff.ino
@@ -1,9 +1,11 @@
 #include <SoftwareSerial.h>
+
 #include <Adafruit_AS7341.h>
+
 #include <Servo.h>
 
 // Variables for bluetooth
-SoftwareSerial Bluetooth(3,2);
+SoftwareSerial Bluetooth(3, 2);
 
 // Configuration and variables for LIDAR
 SoftwareSerial Lidar(4, 5); //define software serial port name as Serial1 and define pin2 as RX and pin3 as TX
@@ -17,85 +19,105 @@ const int HEADER = 0x59; //frame header of data package
 String lidarQueryResponse;
 String LidarQueryResponse;
 String LightQueryResponse;
+int val; // define numeric variables val
+
+int hallPin = 10;
 
 Adafruit_AS7341 as7341;
 
 Servo servo;
-int pos = 0; 
+int pos = 0;
 
 void setup() {
-  
+
   // Begin Serial and SoftwareSerial ports
   Serial.begin(9600);
   Lidar.begin(115200);
   Bluetooth.begin(9600);
-  
+  pinMode(hallPin, INPUT_PULLUP);
+
   servo.attach(9);
   // Listen on Bluetooth port
   Bluetooth.listen();
   // Begin Light sensor
-  if (!as7341.begin()){
+  if (!as7341.begin()) {
     Serial.println("Could not find AS7341");
-    while (1) { delay(10); }
+    while (1) {
+      delay(10);
+    }
   }
   // Light sensor config
   as7341.setATIME(100);
   as7341.setASTEP(999);
   as7341.setGain(AS7341_GAIN_256X);
+  as7341.setLEDCurrent(100);
   as7341.enableLED(false);
   Serial.println("Initialized all sensors, waiting for connections...");
+  Bluetooth.listen();
 }
 
 void loop() {
   // Whilst reciving bluetooth messages
-  if(Bluetooth.available()>0)
-  {
+  if (Bluetooth.available()) {
     Serial.println("made it");
     char data = Bluetooth.read();
     String sData = String(data);
     Serial.println(sData);
     // If query is for lidar (letter 'A'), return lidar stats
-    if (sData == "a")
-    {
+    if (sData == "l") {
       fetchLidar();
-      
+
       LidarQueryResponse = "Distance: " + String(lidarDist) + ":" + String(lidarStrength);
       Bluetooth.print(LidarQueryResponse);
     }
     // Elif query is for light wavelength sensor (letter 'B'), return light stats
-    else if(sData == "b")
-    {
-//      Serial.println("Else if B");
-       fetchLight();
+    else if (sData == "c") {
+      //      Serial.println("Else if B");
+      fetchLight();
 
+      LightQueryResponse = String();
+      int32_t R = int32_t(convertToRgb(as7341.getChannel(AS7341_CHANNEL_680nm_F8)));
+      int32_t G = int32_t(convertToRgb(as7341.getChannel(AS7341_CHANNEL_515nm_F4)));
+      int32_t B = int32_t(convertToRgb(as7341.getChannel(AS7341_CHANNEL_445nm_F2)));
 
-      
-       LightQueryResponse = String(as7341.getChannel(AS7341_CHANNEL_630nm_F7)) + "," + String(as7341.getChannel(AS7341_CHANNEL_515nm_F4)) + "," +String(as7341.getChannel(AS7341_CHANNEL_415nm_F1));
-       Bluetooth.print("#00FF00");
-//       Bluetooth.print(LightQueryResponse);
-    }
-    else if(sData == "c")
-    {
+      LightQueryResponse = RgbToHex(byte(R), byte(G), byte(B));
+      Bluetooth.print(LightQueryResponse);
+    } else if (sData == "s") {
       Serial.println("Servo");
-      Bluetooth.print("ok");
+      Bluetooth.print("Servo: OK");
       for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
         // in steps of 1 degree
-    servo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    servo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-        
+        servo.write(pos); // tell servo to go to position in variable 'pos'
+        delay(15); // waits 15ms for the servo to reach the position
+      }
+      for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+        servo.write(pos); // tell servo to go to position in variable 'pos'
+        delay(15); // waits 15ms for the servo to reach the position
+      }
+
+    } else if (sData == "h") {
+      Serial.println("Hall Sensor");
+      val = digitalRead(hallPin); // read sensor line
+      if (val == LOW) {
+        Bluetooth.print("Near a magnet!");
+      } else {
+        Bluetooth.print("Not near a magnet.");
+      }
+
+    } else if (sData == "r") {
+      fetchLight();
+
+      Bluetooth.print(" F1:" + String(as7341.getChannel(AS7341_CHANNEL_415nm_F1)) + " \n\rF2:" + String(as7341.getChannel(AS7341_CHANNEL_445nm_F2)) + " \n\rF3:" + String(as7341.getChannel(AS7341_CHANNEL_480nm_F3)) + " \n\rF4:" + String(as7341.getChannel(AS7341_CHANNEL_515nm_F4)) + " \n\rF5:" + String(as7341.getChannel(AS7341_CHANNEL_555nm_F5)) + " \n\rF6:" + String(as7341.getChannel(AS7341_CHANNEL_590nm_F6)) + " \n\rF7:" + String(as7341.getChannel(AS7341_CHANNEL_630nm_F7)) + " \n\rF8:" + String(as7341.getChannel(AS7341_CHANNEL_680nm_F8)));
+    } else if (sData = "t") {
+
     }
   }
-  
+
 }
 
-void fetchLidar(){
+void fetchLidar() {
   Lidar.listen();
-  delay(50);
+  delay(75);
   Serial.println("Fetching lidar stats");
   if (Lidar.available()) { //check if serial port has data input
     Serial.println("Lidar available");
@@ -107,26 +129,27 @@ void fetchLidar(){
           uart[i] = Lidar.read();
         }
         check = uart[0] + uart[1] + uart[2] + uart[3] + uart[4] + uart[5] + uart[6] + uart[7];
-        //if (uart[8] == (check & 0xff)) { //verify the received data as per protocol
+        if (uart[8] == (check & 0xff)) { //verify the received data as per protocol
           Serial.println(String(uart[2]));
           Serial.println(String(uart[3]));
           lidarDist = uart[2] + uart[3] * 256; //calculate distance value
           lidarStrength = uart[4] + uart[5] * 256; //calculate signal strength value
           lidarTemprature = uart[6] + uart[7] * 256; //calculate chip temprature
           lidarTemprature = lidarTemprature / 8 - 256;
-        //}
+        }
       }
     }
-    Serial.println("Distance: "+String(lidarDist));
+    Serial.println("Distance: " + String(lidarDist));
     Bluetooth.listen();
-  }  
+  }
 }
 
-
-void fetchLight(){
-  if (!as7341.readAllChannels()){
-      Serial.println("ERROR reading channels in light sensor!");
-      return;
+void fetchLight() {
+  as7341.enableLED(true);
+  delay(50);
+  if (!as7341.readAllChannels()) {
+    Serial.println("ERROR reading channels in light sensor!");
+    return;
   }
   Serial.print("F1 415nm : ");
   Serial.println(as7341.getChannel(AS7341_CHANNEL_415nm_F1));
@@ -150,5 +173,19 @@ void fetchLight(){
 
   Serial.print("Near IR  : ");
   Serial.println(as7341.getChannel(AS7341_CHANNEL_NIR));
+  as7341.enableLED(false);
   return;
+}
+
+String RgbToHex(byte R, byte G, byte B) {
+  char hex[7] = {
+    0
+  };
+  sprintf(hex, "%02X%02X%02X", R, G, B); //convert to an hexadecimal string. Lookup sprintf for what %02X means.
+  return "#" + String(hex);
+}
+
+int convertToRgb(uint16_t x) {
+  Serial.println("Out: " + String(map(x, 0, 65535, 0, 255)) + "in:" + String(x));
+  return map(x, 0, 65535, 0, 255);
 }
